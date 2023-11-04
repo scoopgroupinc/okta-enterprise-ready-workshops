7fa75d9faadddbecf22de79c5be6e2cdb714fc0e
Natalie Pan <natalie.pan@gmail.com>
2023-11-03 20:28:24 -0700

    https://developer.okta.com/blog/2023/07/28/oidc_workshop openID Connect tutorial

--- apps/todo-app/src/app/components/authState.tsx
+++ apps/todo-app/src/app/components/authState.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useCallback, useContext, useState } from 'react';
+import {
+  ReactNode,
+  createContext,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
 
 export interface AuthState {
   name: string | undefined;
@@ -7,90 +13,124 @@ export interface AuthState {
 
 const defaultAuthState: AuthState = {
   name: undefined,
-  isAuthenticated: false
-}
+  isAuthenticated: false,
+};
 
 export type AuthContextType = {
   authState: AuthState;
   userIsAuthenticatedFn: () => Promise<void>;
   onAuthenticateFn: (username: string, password: string) => Promise<void>;
+  onUsernameEnteredFn: (username: string) => Promise<number | null>;
   onRevokeAuthFn: () => Promise<void>;
-}
+};
 
 const defaultAuthContext: AuthContextType = {
   authState: defaultAuthState,
   userIsAuthenticatedFn: async () => {},
   onAuthenticateFn: async () => {},
-  onRevokeAuthFn: async () => {}
-}
+  onUsernameEnteredFn: async () => null,
+  onRevokeAuthFn: async () => {},
+};
 
 const AuthContext = createContext<AuthContextType>(defaultAuthContext);
 export const useAuthState = () => useContext(AuthContext);
 
 type Props = {
   children: ReactNode;
-}
+};
 
 const AuthContextProvider: React.FC<Props> = ({ children }) => {
   const [authState, setAuthState] = useState<AuthState>(defaultAuthState);
 
   const userIsAuthenticatedFn = useCallback(async () => {
     const url = `/api/users/me`;
-      try {
-        const res = await fetch(url, {
-            credentials: 'same-origin',
-            mode: 'same-origin'
-        });
+    try {
+      const res = await fetch(url, {
+        credentials: 'same-origin',
+        mode: 'same-origin',
+      });
 
-        if (res.status === 200) {
-          const {name} = await res.json();
-          setAuthState({name, isAuthenticated: true});
-          localStorage.setItem('isAuthenticated', 'true');
-        } else {
-          setAuthState(defaultAuthState);
-          localStorage.setItem('isAuthenticated', 'false');
-        }
-      } catch (error: unknown) {
+      if (res.status === 200) {
+        const { name } = await res.json();
+        setAuthState({ name, isAuthenticated: true });
+        localStorage.setItem('isAuthenticated', 'true');
+      } else {
         setAuthState(defaultAuthState);
-        console.error(error);
+        localStorage.setItem('isAuthenticated', 'false');
       }
+    } catch (error: unknown) {
+      setAuthState(defaultAuthState);
+      console.error(error);
+    }
   }, [setAuthState]);
 
-  const onAuthenticateFn = async (username: string, password: string) => { 
+  const onAuthenticateFn = async (username: string, password: string) => {
     const url = `/api/signin`;
     try {
-        const res = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ username, password })
-        });
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
 
-        const {name} = await res.json();
+      const { name } = await res.json();
 
-        setAuthState({name, isAuthenticated: true})
-        localStorage.setItem('isAuthenticated', 'true');
-      } catch (error: unknown) {
-        console.error(error);
-      }
-  }
+      setAuthState({ name, isAuthenticated: true });
+      localStorage.setItem('isAuthenticated', 'true');
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  const onUsernameEnteredFn = async (username: string) => {
+    const url = `/api/openid/check`;
+    try {
+      const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username }),
+      });
+
+      const { org_id } = await res.json();
+
+      return org_id;
+    } catch (error: unknown) {
+      console.error(error);
+    }
+    return null;
+  };
 
   const onRevokeAuthFn = async () => {
     const url = `/api/signout`;
     try {
-        const res = await fetch(url, {
-          method: 'POST'
-        });
-        
-        setAuthState(defaultAuthState);
-        localStorage.setItem('isAuthenticated', 'false');
-      } catch (error: unknown) {
-        console.error(error);
-    }
-  }
+      const res = await fetch(url, {
+        method: 'POST',
+      });
 
-  return <AuthContext.Provider value={{ authState, onAuthenticateFn, onRevokeAuthFn, userIsAuthenticatedFn }}>{children}</AuthContext.Provider>;
+      setAuthState(defaultAuthState);
+      localStorage.setItem('isAuthenticated', 'false');
+    } catch (error: unknown) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authState,
+        onUsernameEnteredFn,
+        onAuthenticateFn,
+        onRevokeAuthFn,
+        userIsAuthenticatedFn,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContextProvider;
--- apps/todo-app/src/app/components/signin.tsx
+++ apps/todo-app/src/app/components/signin.tsx
@@ -6,11 +6,26 @@ export const Signin = () => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  const { onAuthenticateFn } = useAuthState();
+  const { onAuthenticateFn, onUsernameEnteredFn } = useAuthState();
   const navigate = useNavigate();
 
-  const onAuthenticate = async () => { 
+  const onAuthenticate = async () => {
     const signIn = async () => {
+      // When user enters just the email but no password. check if the user is part of an org
+      if (username && !password) {
+        console.log('username entered but no password', username);
+        const org_id = await onUsernameEnteredFn(username);
+        console.log(org_id);
+        if (org_id) {
+          window.location.assign(
+            `http://localhost:3333/openid/start/${org_id}`
+          );
+          return;
+        } else {
+          document.getElementById('password-field')?.removeAttribute('hidden');
+          return;
+        }
+      }
       if (username && password) {
         await onAuthenticateFn(username, password);
       }
@@ -22,48 +37,54 @@ export const Signin = () => {
     signIn();
   };
 
-    return (
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <div className="mb-6">
-          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-            Email address
-          </label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className="w-full text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200"
-            value={username}
-            onChange={(event) => setUsername(event.currentTarget.value)}
-          />
-        </div>
-        
-        <div className="mb-6">
-          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-            Password
-          </label>
-          <input
-            id="password"
-            name="email"
-            type="password"
-            autoComplete="current-password"
-            required
-            className="w-full text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200"
-            value={password}
-            onChange={(event) => setPassword(event.currentTarget.value)}
-          />
-        </div>
-        <button 
-            className="w-full py-2 px-3 bg-slate-300 rounded-md"
-            onClick={onAuthenticate}
-            disabled = {!username || !password}
-          >
-            Sign in
-          </button>
+  return (
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div className="mb-6">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Email address
+        </label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="email"
+          required
+          className="w-full text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200"
+          value={username}
+          onChange={(event) => setUsername(event.currentTarget.value)}
+        />
       </div>
-    );
-  }
-  
-  export default Signin;
+
+      <div id="password-field" className="mb-6">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium leading-6 text-gray-900"
+        >
+          Password
+        </label>
+        <input
+          id="password"
+          name="email"
+          type="password"
+          autoComplete="current-password"
+          required
+          className="w-full text-slate-900 placeholder-slate-400 rounded-md py-2 pl-2 ring-1 ring-slate-200"
+          value={password}
+          onChange={(event) => setPassword(event.currentTarget.value)}
+        />
+      </div>
+      <button
+        className="w-full py-2 px-3 bg-slate-300 rounded-md"
+        onClick={onAuthenticate}
+        disabled={!username}
+      >
+        Sign in
+      </button>
+    </div>
+  );
+};
+
+export default Signin;
